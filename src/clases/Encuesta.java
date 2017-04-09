a9339fc41f361f0c002e165afbe102b0ab5a5873
Pol Moya Betriu <moyapol@gmail.com>
2017-04-09 13:33:59 +0200

    Clase Encuesta: Atributos y Getters & Setters

--- src/clases/Encuesta.java
+++ src/clases/Encuesta.java
@@ -7,6 +7,32 @@ import java.util.*;
  */
 public class Encuesta {
 
+    private int id;
+    private String name;
+    private Date data_creacio;
+    private ArrayList<Pregunta>;
 
+    public Encuesta(String name) {
+        this.name = name;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getData_creacio() {
+        return data_creacio;
+    }
+
+    public void setData_creacio(Date data_creacio) {
+        this.data_creacio = data_creacio;
+    }
 }
