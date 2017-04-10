8e87c65b3a204bad7b65beebf381d60bd9d99820
Pol Moya Betriu <moyapol@gmail.com>
2017-04-10 10:31:16 +0200

    Getters Setters y Alguna Operación

--- src/clases/Encuesta.java
+++ src/clases/Encuesta.java
@@ -5,34 +5,77 @@ import java.util.*;
 /**
  *   Clase Encuesta
  */
+
+
 public class Encuesta {
 
     private int id;
     private String name;
-    private Date data_creacio;
-    private ArrayList<Pregunta>;
+    private Calendar creation_date;
+    private Set<Pregunta> ll_preguntes;
 
-    public Encuesta(String name) {
+    private static int id_count = 1;
+
+
+    public Encuesta() {}
+
+    public Encuesta(String name, Calendar data,  Set<Pregunta> llpreguntes) {
         this.name = name;
+        this.id = id_count;
+        ++id_count;
+        this.creation_date = data;
+        this.ll_preguntes = llpreguntes;
     }
 
-    public int getId() {
+    public int getId() {    //GetterId
         return id;
     }
 
-    public String getName() {
+    public String getName() {   //GetterName
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {  //SetterName
         this.name = name;
     }
 
-    public Date getData_creacio() {
-        return data_creacio;
+    public Calendar getCreation_date() {    //GetCreation
+        return creation_date;
     }
 
-    public void setData_creacio(Date data_creacio) {
-        this.data_creacio = data_creacio;
+    public void setCreation_date(Calendar creation_date) {  //SetterCreation
+        this.creation_date = creation_date;
     }
+
+    public Set<Pregunta> getLlista_preguntes() {
+        return ll_preguntes;
+    }
+
+    public void setLlista_preguntes(Set<Pregunta> llpreguntes) {
+        this.ll_preguntes = llpreguntes;
+    }
+
+
+    public boolean eliminar_pregunta(Pregunta pregunta) {
+    //Elimina una pregunta de la encuesta, devuelve true si la elimina, falso si no contiene la pregunta.
+        boolean b =  ll_preguntes.remove(pregunta);
+    }
+
+
+    public boolean modificar_enunciado_pregunta(Pregunta pregunta_old, Pregunta pregunta_new) {
+    //Reemplaza una pregunta por otra, devuelve true si la modifica, false en caso contrario.
+    }
+
+
+    public boolean añadir_pregunta(Pregunta pregunta) {
+    //Añade una pregunta al final de la encuesta, devuelve true si la añade, false si ya existia.
+        return ll_preguntes.add(pregunta);
+    }
+
+    public boolean modificar_pregunta(Pregunta pregunta_old, Pregunta pregunta_new){}
+
+
+
+
+
 }
--- src/clases/Encuesta_Respondida.java
+++ src/clases/Encuesta_Respondida.java
@@ -7,45 +7,18 @@ import java.util.*;
 public class Encuesta_Respondida {
 
 
+    private int id_encuesta;
+    private String nombre;
+    private Calendar date;
+    private int[]  llista_respuestas;
 
-    public String nombre_encuesta_respondida;
-    public String fecha_encuesta_respondida;
-    public int[]  vector_encuesta_respondida;
-    public int id_encuesta;
 
 
-    public Encuesta_Respondida() {
-    }
+    public Encuesta_Respondida() {}
 
-    public Encuesta_Respondida(String nombre_encuesta_respondida, String fecha_encuesta_respondida, int[] vector_encuesta_respondida, int id_encuesta) {
-        this.nombre_encuesta_respondida = nombre_encuesta_respondida;
-        this.fecha_encuesta_respondida = fecha_encuesta_respondida;
-        this.vector_encuesta_respondida = vector_encuesta_respondida;
-        this.id_encuesta = id_encuesta;
-    }
-
-    public String getNombre_encuesta_respondida() {
-        return nombre_encuesta_respondida;
-    }
-
-    public void setNombre_encuesta_respondida(String nombre_encuesta_respondida) {
-        this.nombre_encuesta_respondida = nombre_encuesta_respondida;
-    }
-
-    public String getFecha_encuesta_respondida() {
-        return fecha_encuesta_respondida;
-    }
-
-    public void setFecha_encuesta_respondida(String fecha_encuesta_respondida) {
-        this.fecha_encuesta_respondida = fecha_encuesta_respondida;
-    }
-
-    public int[] getVector_encuesta_respondida() {
-        return vector_encuesta_respondida;
-    }
-
-    public void setVector_encuesta_respondida(int[] vector_encuesta_respondida) {
-        this.vector_encuesta_respondida = vector_encuesta_respondida;
+    public Encuesta_Respondida(String nombre, int[] llista_respuestas) {
+        this.nombre = nombre;
+        this.llista_respuestas = llista_respuestas;
     }
 
     public int getId_encuesta() {
@@ -55,4 +28,34 @@ public class Encuesta_Respondida {
     public void setId_encuesta(int id_encuesta) {
         this.id_encuesta = id_encuesta;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public int[] getLlista_respuestas() {
+        return llista_respuestas;
+    }
+
+    public void setLlista_respuestas(int[] llista_respuestas) {
+        this.llista_respuestas = llista_respuestas;
+    }
+
+
 }
+
+
+
+
