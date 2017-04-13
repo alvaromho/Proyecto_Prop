980ce4f5ea0f84e1c7a0151e41ef68bfa9e334fb
Alvaor Muñoz <alvaro.mho2@gmail.com>
2017-04-13 18:25:07 +0200

    Funcion distancia R_texto

--- src/clases/R_Texto.java
+++ src/clases/R_Texto.java
@@ -64,6 +64,50 @@ public class R_Texto extends Respuesta {
 
     @Override
     public float distancia(Respuesta respuesta) {
-        return super.distancia(respuesta);
+
+        String valor_1 =this.valor;
+        String valor_2 = (String) respuesta.getValor();
+
+        return (float) Lev(valor_1,valor_2) / (float) max(valor_1.length(), valor_2.length());
+
+    }
+
+
+    public static int Lev(String s1, String s2) {
+        int i = s1.length();
+        int j = s2.length();
+        // Si alguno de los strings esta vacio retornar 0
+        if (i == 0 || j == 0){
+            return (i >= j) ? i : j;
+        }
+        return Levenshtein(s1.toCharArray(),s2.toCharArray());
+
+    }
+
+    private static int Levenshtein(char [] s1, char [] s2) {
+        int [][]distance = new int[s1.length+1][s2.length+1];
+        for(int i=0;i<=s1.length;i++){
+            distance[i][0]=i;
+        }
+        for(int j=0;j<=s2.length;j++){
+            distance[0][j]=j;
+        }
+        for(int i=1;i<=s1.length;i++){
+            for(int j=1;j<=s2.length;j++){
+                distance[i][j]= min(distance[i-1][j]+1,
+                                    distance[i][j-1]+1,
+                                    distance[i-1][j-1]+ ((s1[i-1]==s2[j-1])?0:1));
+            }
+        }
+        return distance[s1.length][s2.length];
+
+    }
+
+    public static int min(int a, int b, int c){
+        return (a <= b) ? (a <= c ) ? a : c : (b <= c) ? b : c;
+    }
+
+    public  static int max(int a, int b) {
+        return (a >= b) ? a : b;
     }
 }
