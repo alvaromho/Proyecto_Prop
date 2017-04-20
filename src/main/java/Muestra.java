a0aea35cce4f8800ca5c7dcc963504fb29e2c701
Paula Alemany <paulaalemany1995@gmail.com>
2017-04-20 20:03:31 +0200

    Finalizacion Encuesta y Encuesta Respondida y alguna cosa sobre pregunta y subclasses y respuesta y subclasses

--- src/main/java/Muestra.java
+++ src/main/java/Muestra.java
@@ -4,25 +4,30 @@ import java.lang.*;
 /**
  * Paula Alemany i Vergé
  */
+
 public class Muestra {
-    /*Atributos*/
+    /**
+     * Atributos
+     */
     private int id_muestra;
     private static int contador = 1;
     private ArrayList<Encuesta_Respondida> lista_respuestas = new ArrayList();
     private Encuesta encuesta;
 
     /**
-     * Creadora
+     * Creadora a partir de pasar como parametro una encuesta que a partir de ella podremos obtener la lista de encuestas respondidas de esa misma encuesta.
+     * @param e
      */
-
     public Muestra (Encuesta e){
         this.setId_muestra();
         this.lista_respuestas = e.getLl_encuestas_respondidas();
-        /*System.out.println("Introduce el nombre de la encuesta que se quiere para la muestra:");
-        Scanner scanner = new Scanner(System.in);
-        String nombre = scanner.next();*/
     }
 
+    /**
+     * Creadora a partir de una encuesta y una lista de encuestas respondidas de la cual sacaremos las que tengan el mimso nombre de encuesta.
+     * @param e
+     * @param lista_encuestas_respondida
+     */
     public Muestra(Encuesta e, ArrayList<Encuesta_Respondida> lista_encuestas_respondida ) {
         this.setId_muestra();
         this.encuesta = e;
@@ -36,6 +41,13 @@ public class Muestra {
         }
     }
 
+    /**
+     * Creadora con filtro a partir de una pregunta y el valor de su respuesta.
+     * @param e
+     * @param lista_encuestas_respondida
+     * @param pregunta
+     * @param valor
+     */
     public Muestra(Encuesta e, ArrayList<Encuesta_Respondida> lista_encuestas_respondida, Pregunta pregunta, Object valor) {
         for (Encuesta_Respondida er : lista_encuestas_respondida){
             for (Respuesta r : er.getLl_respuesta()) {
@@ -48,40 +60,57 @@ public class Muestra {
         }
     }
 
-
+    /**
+     * Devuelve el nombre de la encuesta a la que corresponde la encuestas respondidas que tomamos como muestra
+     * @return El nombre de la Encuesta.
+     */
     public String consultar_nombre_encuesta(){
         return this.encuesta.getName();
     }
 
+    /**
+     * Devuelve la lista de encuestas respondidas que tenemos en esta muestra.
+     * @return lista de encuestas respondidas
+     */
     public ArrayList<Encuesta_Respondida> get_lista_respuesta(){
         return this.lista_respuestas;
     }
 
+    /**
+     * Modifica la lista de respuestas que tenemos como atributo de la clase Muestra por una que pasamos como parametro.
+     * @param EncuestasRespondidas
+     */
     public void set_lista_respuestas(ArrayList<Encuesta_Respondida> EncuestasRespondidas){
         this.lista_respuestas = EncuestasRespondidas;
     }
 
-
+    /**
+     * Devuelve el atributo id_muestra de esta clase.
+     * @return id_muestra
+     */
     public int getId_muestra() {
         return id_muestra;
     }
 
+    /**
+     * Modifica el valor del atributo id_muestra a partir de un contador que inicializamos a 1 y al assignar el valor al id automaticamente el valor del contador se incrementa.
+     */
     public void setId_muestra() {
         this.id_muestra = contador++;
     }
 
-    public ArrayList<Encuesta_Respondida> getLista_respuestas() {
-        return lista_respuestas;
-    }
-
-    public void setLista_respuestas(ArrayList<Encuesta_Respondida> lista_respuestas) {
-        this.lista_respuestas = lista_respuestas;
-    }
-
+    /**
+     * Devuelve la encuesta que tenemos en esta clase.
+     * @return Encuesta
+     */
     public Encuesta getEncuesta() {
         return encuesta;
     }
 
+    /**
+     * Modifica la encuesta que tenemos en esta clase a partir de otra clase que pasamos como parametro.
+     * @param encuesta
+     */
     public void setEncuesta(Encuesta encuesta) {
         this.encuesta = encuesta;
     }
